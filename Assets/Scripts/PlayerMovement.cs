1bfe1606f1aae7116664ed03873f11516e478762
Darrel Odom <ddodom17@gmail.com>
2022-10-29 19:59:22 -0500

    Added dash functionality to player
    added dash and tweaked the jump a bit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Components")]
     private Rigidbody2D rb;
+    private TrailRenderer trailRenderer;
+    private Animator animator;
 
     [Header("Movement Variables")]
     [SerializeField] private float maxMoveSpeed = 10f;
+    private float originalMoveSpeed = 10f;
     private float moveDirectionX;
     private float moveDirectionY;
 
@@ -22,6 +25,13 @@ public class PlayerMovement : MonoBehaviour
     private int jumpCounter = 0;
     [SerializeField] private float currentJumpTime;
 
+    [Header("Dash Variables")]
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashSpeed = 20f;
+    private Vector2 dashingDir;
+    private bool isDashing;
+    private bool canDash = true;
+
     [Header("Layer Masks")]
     [SerializeField] private LayerMask Ground;
 
@@ -29,20 +39,26 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float groundRayLength;
     public bool isGrounded;
     public bool jumpReleased;
+    private bool dashInput;
     private bool isJumping;
 
 
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        trailRenderer = GetComponent<TrailRenderer>();
+        //animator = GetComponent<Animator>();
     }
 
     
     void Update()
     {
+        moveDirectionY = GetInput().y;
         moveDirectionX = GetInput().x;
         jumpReleased = Input.GetButtonUp("Jump");
+        dashInput = Input.GetButtonDown("Dash");
         Jump();
+        Dash();
     }
 
     private void FixedUpdate()
@@ -54,10 +70,12 @@ public class PlayerMovement : MonoBehaviour
             jumpCounter = 0;
             currentJumpTime = 0f;
             maxJumpTime = 0.35f;
+            canDash = true;
         }
 
         MoveCharacter();
         PlayerFall();
+        
     }
 
     private Vector2 GetInput()
@@ -69,6 +87,15 @@ public class PlayerMovement : MonoBehaviour
     {
         float moveDirectionX = Input.GetAxisRaw("Horizontal") * maxMoveSpeed;
 
+        if (moveDirectionX > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (moveDirectionX < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+            
         // Velocity Settings
         Vector2 newVelocity;
 
@@ -81,7 +108,8 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump") && isGrounded == true)
         {
-            isJumping = false;
+            isJumping = true;
+            //currentJumpTime = 0f;
             Vector2 newVelocity;
             newVelocity.x = rb.velocity.x;
             newVelocity.y = jumpForce;
@@ -101,29 +129,28 @@ public class PlayerMovement : MonoBehaviour
             currentJumpTime += Time.deltaTime;
         }
 
-        if (Input.GetButton("Jump"))
+        if (Input.GetButton("Jump") && isJumping == true)
         {
-            currentJumpTime += Time.deltaTime;
-
             if (currentJumpTime < maxJumpTime)
             {
                 Vector2 newVelocity;
                 newVelocity.x = rb.velocity.x;
                 newVelocity.y = jumpForce;
                 rb.velocity = newVelocity;
+                currentJumpTime += Time.deltaTime;
             }
 
-            else if (currentJumpTime >= maxJumpTime && rb.velocity.y > 0)
+            else if (currentJumpTime >= maxJumpTime)
             {
-                //rb.gravityScale = fallMultiplier;
                 rb.velocity = new Vector2(rb.velocity.x, 0);
-                //isJumping = true;
+                isJumping = false;
             }
         }
 
         if (jumpReleased && rb.velocity.y > 0)
         {
-            currentJumpTime = 0f;
+            jumpReleased = true;
+            isJumping = false;
             rb.velocity = new Vector2(rb.velocity.x, 0);
         }
         
@@ -145,12 +172,55 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void Dash()
+    {
+        float originalGravity = rb.gravityScale;
+
+        if (dashInput && canDash)
+        {
+            rb.gravityScale = 0f;
+            isDashing = true;
+            canDash = false;
+            trailRenderer.emitting = true;
+            dashingDir = GetInput();
+        
+            if (dashingDir == Vector2.zero)
+            {
+                dashingDir = new Vector2(moveDirectionX, 0);
+            }
+
+            StartCoroutine(StopDashing());
+        }
+
+        if (isDashing)
+        {
+            rb.velocity = dashingDir * dashSpeed;
+            return;
+        }
+
+        if (isGrounded)
+        {
+            rb.gravityScale = originalGravity;
+            canDash = true;
+        }
+    }
+
     public bool GroundCheck()
     {
         LayerMask Ground = LayerMask.GetMask("Ground");
         RaycastHit2D hitRec = Physics2D.Raycast(transform.position * groundRayLength, Vector2.down, groundRayLength, Ground);
 
         return hitRec.collider != null;
+    }
+
+    private IEnumerator StopDashing()
+    {
+        yield return new WaitForSeconds(dashTime);
+        trailRenderer.emitting = false;
+        isDashing = false;
+        
+        if (isDashing == false)
+            maxMoveSpeed = originalMoveSpeed;
     }
 
     private void OnDrawGizmos()
